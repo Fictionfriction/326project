cde7c88a0021d45f2b1b2227016d530eb49c4d71
Jason Lim <jasonlim@umd.edu>
2021-11-14 16:23:14 -0500

    added player class and player methods

---
+++ player.py
@@ -0,0 +1,25 @@
+class Player:
+    """
+    A player in the maze game
+    
+    Atrributes:
+        name (str): the player's name
+        health (int): the health that a player has to get through the maze
+        score (int): the player's score for completing the maze
+    """
+    
+    def __init__(self, name):
+        self.name = name
+        self.health = 3
+    
+    def player_hurt(self):
+        self.health = self.health - 1
+    
+    def player_health(self):
+        self.health = self.health + 1
+        
+    def check_name(self):
+        if len(self.name) > 3:
+            return(print("Please make sure name is 3 characters long!"))
+        else:
+            return self.name
